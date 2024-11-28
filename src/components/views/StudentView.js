2757168aff9a34e1113d91a0c67aa16841247aae
anitaprova <104943181+anitaprova@users.noreply.github.com>
2024-11-28 15:31:41 -0500

    added delete button

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,14 +4,15 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import "../css/StudentView.css";
 
 const StudentView = (props) => {
-  const { student } = props;
+  const { student, deleteStudent } = props;
 
   // Render a single Student view
   return (
-    <div class="container">
+    <div className="container">
       <img src={student.imageURL} alt="Default" />
       <h1>{student.firstname + " " + student.lastname}</h1>
       <h3>
@@ -21,6 +22,11 @@ const StudentView = (props) => {
       </h3>
       <p>Email: {student.email}</p>
       <p>GPA: {student.gpa}</p>
+      <Link to={'/students'}>
+        <button onClick={() => deleteStudent(student.id)}>
+          Delete Student
+        </button>
+      </Link>
     </div>
   );
 };
--- src/store/reducers/student.js
+++ src/store/reducers/student.js
@@ -5,7 +5,7 @@ This is a Reducer function that accepts 2 parameters: the previous state object 
 Depending on the Action object, the Reducer updates the State and return the new State object.
 It also defines the State and its default initial value.
 ================================================== */
-import { FETCH_STUDENT } from "../actions/actionTypes";  // Import Action Type
+import { FETCH_STUDENT, DELETE_STUDENT } from "../actions/actionTypes";  // Import Action Type
 
 // Define default Initial state
 const initialState = {
@@ -17,6 +17,8 @@ const student = (state=initialState, action) => {  // Use "initialState" as defa
   switch (action.type) {
     case FETCH_STUDENT:
       return action.payload;
+    case DELETE_STUDENT:
+      return state.filter((student) => student.id !== action.payload);
     default:
       // If the Reducer doesn't recognize the Action Type, returns the previous (current) State unchanged.
       return state;
