a591c39002e19cdf309e5e4d97affaf6447979f3
[tes] <tesneeme11@gmail.com>
2024-12-11 01:04:29 -0500

    fix delete campus

--- src/store/reducers/campus.js
+++ src/store/reducers/campus.js
@@ -5,7 +5,7 @@ This is a Reducer function that accepts 2 parameters: the previous state object 
 Depending on the Action object, the Reducer updates the State and return the new State object.
 It also defines the State and its default initial value.
 ================================================== */
-import { FETCH_CAMPUS, DELETE_CAMPUS } from "../actions/actionTypes";  // Import Action Type
+import { FETCH_CAMPUS } from "../actions/actionTypes";  // Import Action Type
 
 // Define default Initial State
 const initialState = {
@@ -17,8 +17,6 @@ const campus = (state = initialState, action) => {  // Use "initialState" as def
   switch (action.type) {
     case FETCH_CAMPUS:
       return action.payload;
-      // case DELETE_CAMPUS:
-      //   return state.filter((campus) => campus.id !== action.payload);
     default:
       // If the Reducer doesn't recognize the Action Type, returns the previous (current) State unchanged.
       return state;
